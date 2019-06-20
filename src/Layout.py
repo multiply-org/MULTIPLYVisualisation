fa2c5e100d1a5cfe784b023b16d6e7d3e6b62d47
BethanPerkins <bethan.perkins@assimila.eu>
2019-06-20 12:20:21 +0100

    layout started to copy the wireframe

--- src/Layout.py
+++ src/Layout.py
@@ -7,44 +7,115 @@ class Layout:
     def index(plotter):
 
         page = html.Div([
-                        html.H1(
-                            children='Multiply Visulisation',
-                            style={'textAlign': 'center'}),
-                        html.Div(
-                            [dcc.Markdown(
-                                id='markdown',
-                                children="default text")]),
-                        plotter.generate_parameter_dropdown(),
+
+             # first logo:
+
+             html.Div(children=[
+
+                 html.Div(id='topleft_logo', children=[
+                     html.Img(src='assets/logo.png'),
+                 ]),
+
+                 html.Div(id='main_title_container', children=[
+                     html.P('MULTIPLY Visualisations',
+                            id='main_title')
+                 ]),
+
+                 html.Div(id='descriptor_container',children=[
+                     html.P('Some filler text',
+                            id='descriptor')
+                 ])
+
+                ])
+             ,
+
+             html.Div(id='data_background', children=[
+
+                 html.Div(id='subtitle_container',children=[
+                    html.P('Data Display:', id='subtitle')
+                    ])
+                    ,
+                 html.Div(id='unc_expl_container', children=[
+                    html.P('Some description.', id='unc_expl')
+                    ])
+                    ,
+                 html.Div(style={'clear': 'left'})
+
+                    ,
+
+                 html.Div(children=[
+
+                    html.Div(id='data_container',children=[
+                        dcc.Graph(id='core-map')
+                        ])
+                     ,
+                    html.Div(id='unc_container', children=[
+                        dcc.Graph(id='unc-map')
+                        ])
+
+                    ])
+                    ,
+
+                 html.Div(children=[
+
+                    html.Div(id='dropdown_container',children=[
+                        dcc.Markdown(
+                            id='markdown',
+                            children="Select Parameter:"),
+                            plotter.generate_parameter_dropdown()
+                        ]),
+                    html.Div(id='button_container',children=[
                         html.Button(
                             id='select',
                             n_clicks=0,
-                            children='Select'),
-                        html.Div(
-                            id='slider-container',
-                            style={
-                                'width': '80%',
-                                'display':'inline-block',
-                                'margin-bottom':'40px',
-                                'height':'40px'},
-                            children=[dcc.Slider(id='time-slider')]),
-                        html.Div(
-                            style={
-                                'width': '40%',
-                                'float':'left',
-                                'display':'inline-block'},
-                            children=[dcc.Graph(id='core-map')]),
-                        html.Div(
-                            style={
-                                'width': '40%',
-                                'float':'left',
-                                'display':'inline-block'},
-                            children=[dcc.Graph(id='unc-map'),]),
-                        html.Div(
-                            style={
-                                'width': '80%',
-                                'float':'left',
-                                'display':'inline-block'},
-                            children=dcc.Graph(id='pixel_timeseries'))
+                            children='Select')
+                        ])
                     ])
+                    ,
+
+                    # html.Div(id='slider_container', children=[
+                    #     dcc.Slider(id='time-slider')
+                    #     ])
+
+
+                 ])
+
+
+                #      html.Div(
+                #          [dcc.Markdown(
+                #              id='markdown',
+                #              children="default text")]),
+                #      plotter.generate_parameter_dropdown(),
+                #      html.Button(
+                #          id='select',
+                #          n_clicks=0,
+                #          children='Select'),
+                #      html.Div(
+                #          id='slider-container',
+                #          style={
+                #              'width': '80%',
+                #              'display':'inline-block',
+                #              'margin-bottom':'40px',
+                #              'height':'40px'},
+                #          children=[dcc.Slider(id='time-slider')]),
+                #      html.Div(
+                #          style={
+                #              'width': '40%',
+                #              'float':'left',
+                #              'display':'inline-block'},
+                #          children=[dcc.Graph(id='core-map')]),
+                #      html.Div(
+                #          style={
+                #              'width': '40%',
+                #              'float':'left',
+                #              'display':'inline-block'},
+                #          children=[dcc.Graph(id='unc-map'),]),
+                #      html.Div(
+                #          style={
+                #              'width': '80%',
+                #              'float':'left',
+                #              'display':'inline-block'},
+                #          children=dcc.Graph(id='pixel_timeseries'))
+                 ])
 
         return page

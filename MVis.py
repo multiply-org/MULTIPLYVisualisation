046762cb17ffbb319305fed526f2e6c97712e489
BethanPerkins <bethan.perkins@assimila.eu>
2019-06-28 11:28:04 +0100

    Adding a little docstring

--- MVis.py
+++ MVis.py
@@ -1,5 +1,16 @@
 from src.MultiplyVis import MultiplyVis
 
+"""
+To run the MULTIPLY visualisation with default data, type the following:
+
+> import MVis
+> MVis.show()
+
+To use a bespoke directory, type:
+> MVis.show(os.path.abspath('path/to/directory')
+
+"""
+
 def show(directory=None):
 
     if directory:
